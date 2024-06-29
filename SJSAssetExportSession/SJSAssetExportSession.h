6d464ff2b82835d2729e3d58fcd07b040ff79e1a
Sami Samhuri <sami@samhuri.net>
2024-06-29 08:18:05 -0700

    Initial Commit

---
+++ SJSAssetExportSession/SJSAssetExportSession.h
@@ -0,0 +1,18 @@
+//
+//  SJSAssetExportSession.h
+//  SJSAssetExportSession
+//
+//  Created by Sami Samhuri on 2024-06-29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SJSAssetExportSession.
+FOUNDATION_EXPORT double SJSAssetExportSessionVersionNumber;
+
+//! Project version string for SJSAssetExportSession.
+FOUNDATION_EXPORT const unsigned char SJSAssetExportSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJSAssetExportSession/PublicHeader.h>
+
+
---
+++ SJSAssetExportSessionTests/SJSAssetExportSessionTests.swift
@@ -0,0 +1,17 @@
+//
+//  SJSAssetExportSessionTests.swift
+//  SJSAssetExportSessionTests
+//
+//  Created by Sami Samhuri on 2024-06-29.
+//
+
+import Testing
+@testable import SJSAssetExportSession
+
+struct SJSAssetExportSessionTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
